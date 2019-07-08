17b1b043a9e75e4512bdf660a3f0530d3a012351
Ibrohim Bahromov <hadus_bi@mail.ru>
2019-07-08 20:05:31 +0500

    Lesson 8

---
+++ lesson_8_javascript/index.js
@@ -0,0 +1,52 @@
+// let name1 = 'Anvar'
+// let name2 = 'Botir'
+// let name3 = 'Sobir'
+
+// name1 = 'Jobir'
+
+// let age1 = 22
+// let age2 = 23
+// let age3 = 26
+
+// let names = ['Anvar', 'Botir', 'Sobir']
+// let ages = [22, 23, 26]
+
+// names[0] = 'Jobir'
+
+// let fruit = 'Shaftoli'
+// let fruit2 = fruit
+
+let person1 = {
+  name: 'Anvar',
+  age: 22,
+  address: {
+    region: 'Toshkent',
+    district: 'Mirobod'
+  }
+}
+
+let temp = person1
+
+person1.name = 'Jobir'
+
+// console.log(temp.name)
+
+// let person2 = {name: 'Botir', age: 23}
+// let person3 = {name: 'Sobir', age: 26}
+
+// let people = [
+//   {name: 'Anvar', age: 22},
+//   {name: 'Botir', age: 23},
+//   {name: 'Sobir', age: 26},
+// ]
+
+// let ozgaruvchi = 'age'
+// console.log(person1.name + ' ' + person1[ozgaruvchi] + ' yoshda.')
+
+// let raqam = '21'
+
+// let yangiRaqam = Number(raqam)
+
+// console.log(yangiRaqam + 1)
+
+console.log(Number.parseInt(3/2))
