6a0fb6798974f8832d0b9412e8012ef37debb336
Ibrohim Bahromov <hadus_bi@mail.ru>
2019-07-16 10:52:56 +0500

    Lesson 11

---
+++ lesson_11_javascript/index.js
@@ -0,0 +1,144 @@
+// function add(a, b) {
+//     return a + b
+// }
+
+// function minus(a, b) {
+//     return a - b
+// }
+
+// function func(foo, boo) {
+//     return foo(3, 4) * boo(3, 4)
+// }
+
+// console.log(func(add, minus))
+
+// Oddiy funksiya
+// console.log(eval('3 * 4 - 1'))
+
+// Obyekt ichidagi funksiya
+// let obj = {
+//     name: 'Jobir',
+//     age: 22,
+//     height: 170,
+//     weight: '90kg'
+// }
+
+// console.log(Object.keys(obj))
+// console.log(Object.values(obj))
+
+// Prototype funksiya
+// let name = 'Anvar'
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+
+// var && let
+
+// function foo() {
+//     var a = 8
+// }
+
+// foo()
+
+// console.log(a)
+
+// let arr1 = [1, 2, 3]
+// let arr2 = new Array(1, 2, 3)
+
+// console.log(arr2)
+
+// let obj1 = {
+//     name: 'Anvar',
+//     age: 23
+// }
+
+// let obj2 = new Object({
+//     name: 'Anvar',
+//     age: 23
+// })
+
+// console.log(obj1)
+
+// let num1 = 7
+// let num2 = 7
+// let num3 = new Number(7)
+// let num4 = new Number(7)
+
+// console.log(num3 === num4)
+
+// let arr = [1, 2, , 4, ]
+
+// console.log(arr.length)
+
+// let num = 3
+// let num2 = 0b11
+// let num8 = 0o3
+// let num16 = 0x3
+
+// let obj = {    
+//     name: 'Anvar', 
+//     5: true, 
+//     '!': 'Empty string',
+//     null: '???',
+//     let: 'jfdks',
+// }
+
+// console.log(obj['let'])
+// console.log(Object.keys(obj))
+
+// let i = 0
+
+// while(i > 10) {
+//     console.log(i)
+// }
+
+// do {
+//     console.log(i)
+// } while(i > 10)
+
+// let arr = [1, 2, 3]
+
+// let one = arr[0]
+// let two = arr[1]
+// let three = arr[2]
+
+// let [one, two, three] = arr
+
+// let obj = {
+//     name: 'Anvar',
+//     age: 22,
+//     height: 180,
+// }
+
+// let name = obj.name
+// let age = obj.age
+
+// let { age, name } = obj
+
+// let num = 5
+
+// console.log(num**5)
+
+// let arr = [1, 2, 3]
+
+// arr.pop()
+
+// delete arr[1]
+
+// arr[1] = 7
+
+// console.log(arr.pop())
+
+// let arr = [1, 2, 3]
+
+// console.log(1 in arr)
+
+// let arr1 = [2, 3, 4]
+// let arr2 = [1, ...arr1, 4, 5]
+
+// let arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+
+// console.log(arr[1][1])
