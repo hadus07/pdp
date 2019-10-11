a5027627929551c1a8be8526a2c681af09cd0ba4
Ibrohim Bahromov <hadus_bi@mail.ru>
2019-10-11 21:02:59 +0500

    Lesson 46

--- storyboard/client/src/screens/home/index.js
+++ storyboard/client/src/screens/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Header } from '../../components/header'
 import { Random } from '../../components/random'
+import { Search } from '../../components/search'
 
 export const Home = () => (
     <>
         <Header />
         <Random />
+        <Search />
     </>
 )
