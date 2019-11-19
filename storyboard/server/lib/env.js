ee08096b335fccecd9a01b7fb2071f27f10607fb
Ibrohim Bahrmov <hadus_bi@mail.ru>
2019-11-19 21:12:12 +0500

    Hato qib qoyippiz

--- storyboard/server/lib/env.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var URL = exports.URL = 'http://localhost:4000';
-var CYPHER = exports.CYPHER = 'hChkimBilmAsiNdbqOyin123123!!@#$%^&*';
