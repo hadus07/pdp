5a0ab5015bd54c9faf427caa1c3cc29e103a8b17
Ibrohim Bahrmov <hadus_bi@mail.ru>
2020-02-17 21:08:49 +0500

    Update helpers.js

--- storyboard/server/src/helpers.js
+++ storyboard/server/src/helpers.js
@@ -3,16 +3,18 @@ import utf8 from 'crypto-js/enc-utf8'
 import { User } from './schemas/users'
 import { encrypt, decrypt } from 'crypto-js/aes'
 
+const OFFSET = 1000 * 60 * 60 * 24 // a day
+
 export const sendError = error => ({ error })
 
 export const generateToken = email =>
-    encrypt(`${email}|${new Date().valueOf() + (3600000 * 24)}`, CYPHER).toString()
+    encrypt(`${email}|${new Date().valueOf() + OFFSET}`, CYPHER).toString()
 
 export const checkToken = async (req, res, next) => {
     let { token } = req.body
 
     if (token) {
-        let [email, expDate] = decrypt(token, CYPHER).toString(utf8).split('|')[0]
+        let [email, expDate] = decrypt(token, CYPHER).toString(utf8).split('|')
 
         if (expDate <= new Date().valueOf()) {
             res.json(sendError('token_expired'))
